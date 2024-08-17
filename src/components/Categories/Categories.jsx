d138f736731682f30dd841bcedefc5fae6cefbd6
magomedov-m <magomedov.dgu@gmail.com>
2024-08-17 19:31:12 +0300

    .

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -6,7 +6,7 @@ import Info from "../Info/Info";
 export default function Categories() {
   const [arr, setArr] = useState([]);
   useEffect(() => {
-    fetch("https://api.github.com/users/magomedov-m/repos")
+    fetch(`https://api.github.com/users/${'magomedov-m'}/repos`)
       .then((res) => res.json())
       .then((result) => {
         setArr(result);
@@ -15,7 +15,7 @@ export default function Categories() {
   console.log(arr);
   return (
     <div>
-        {/* {arr.length == 0 ? null : <Info />} */}
+        {arr.length == 0 ? null : <Info />}
         {arr.length == 0 ? <div className="preview">Добро пожаловать</div> : arr.map(el => <CategotyCard props={el} />)}
     </div>
   );
